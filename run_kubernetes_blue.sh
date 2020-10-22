7a0604f557bbee20c5915c11cbd716fb71d4d724
hamb-ru <hamb@mail.ru>
2020-10-22 22:18:38 +0000

    run_kube scripts updated

--- run_kubernetes_blue.sh
+++ run_kubernetes_blue.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 # Step 1:
-# dockerpath=<>
-dockerpath="hamb/capstone_green"
+# Stop any running Capstone containers
+docker rm -f capstone >> /dev/null 2>&1 
 
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl create deployment capstone --image=$dockerpath
+# Step 2:
+# Set image path
+dockerpath="hamb/capstone_blue"
 
 # Step 3:
+# Run the BLUE Docker container with kubernetes
+kubectl create deployment capstone-blue --image=$dockerpath
+
+# Step 4:
+# Wait 10 seconds till the pod come up
+sleep 10
+
+# Step 5:
 # List kubernetes pods
 kubectl get pods --all-namespaces
 
-# Step 4:
-# Forward the container port 8888 to a host port 8877 (blue deployment)
-kubectl port-forward --address 0.0.0.0 deployment/capstone 8877:8888
+# Step 6:
+# Forward the container port 8888 to a host port 8877 (green deployment)
+nohup kubectl port-forward --address 0.0.0.0 deployment/capstone-blue 8877:8888 &
--- run_kubernetes_green.sh
+++ run_kubernetes_green.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 # Step 1:
-# dockerpath=<>
+# Stop any running Capstone containers 
+docker rm -f capstone >> /dev/null 2>&1 
+
+# Step 2:
+# Set image path
 dockerpath="hamb/capstone_green"
 
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl create deployment capstone --image=$dockerpath
-
 # Step 3:
+# Run the GREEN Docker container with kubernetes
+kubectl create deployment capstone-green --image=$dockerpath
+
+# Step 4:
+# Wait 10 seconds till the pod come up
+sleep 10
+
+# Step 5:
 # List kubernetes pods
 kubectl get pods --all-namespaces
 
-# Step 4:
+# Step 6:
 # Forward the container port 8888 to a host port 8899 (green deployment)
-kubectl port-forward --address 0.0.0.0 deployment/capstone 8899:8888
+kubectl port-forward --address 0.0.0.0 deployment/capstone-green 8899:8888 &
